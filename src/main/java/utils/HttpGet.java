937bc5b74d60c72a48ca51158bf91d8cc3577b75
rserranollamas <92532301+rserranollamas@users.noreply.github.com>
2022-05-26 21:19:08 +0200

    Clase HttpGet modificada
    Se añade fix para ignorar la comprobación del certificado SSL al conectar con la API REST.
    Habría que instalar el certificado del ministerio e importarlo a Java, he comprobado que ocurre en sistemas operativos macOS e instalando diferentes versiones de JDK.
    Con este fix se ignora el paso de tener que instalar manualmente el certificado de la web del ministerio.

--- src/main/java/utils/HttpGet.java
+++ src/main/java/utils/HttpGet.java
@@ -5,9 +5,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class HttpGet {
-	public static String httpGet(String urlStr) throws IOException {
+	public static String httpGet(String urlStr) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+		/* Start of the fix */
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+
+        } };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) { return true; }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        /* End of the fix */
+		
+		
 		// Creamos un objeto de tipo URL
 		URL url = new URL(urlStr);
 		// Abrimos la conexión e indicamos que será de tipo GET HttpURLConnection
