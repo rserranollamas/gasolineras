4fcb2bdc2a583c7c1a32e60d6e93561c6c92e7d9
rserranollamas <92532301+rserranollamas@users.noreply.github.com>
2022-05-26 21:48:25 +0200

    Clase UsuarioDAO modificada
    Se cambian localidadesBusqueda y posicionesBusqueda de ArrayList a HashSet.
    Se modifican las funciones boolean añadirLocalidad, quitarLocalidad, añadirPosicion y quitarPosicion.
    Se crea dentro de la función public TreeSet<Gasolinera> un nuevo objeto TreeSet<Gasolinera>.
    Se modifica el nombre del objeto datos ya que la clase ya no se llama Gasolineras, ahora se llama ListadoGasolineras.

--- src/main/java/clases/UsuarioDAO.java
+++ src/main/java/clases/UsuarioDAO.java
@@ -1,7 +1,9 @@
 package clases;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
 import java.util.TreeSet;
 
 import com.google.gson.Gson;
@@ -12,9 +14,19 @@ public class UsuarioDAO {
 	private String nombre;
 	private String email;
 	private String password;
-	private ArrayList<String> localidadesBusqueda;
-	private ArrayList<PosicionDAO> posicionesBusqueda;
-	private TreeSet<ListaEESSPrecio> gasolineras;
+	private HashSet<String> localidadesBusqueda;
+	private HashSet<PosicionDAO> posicionesBusqueda;
+
+	public UsuarioDAO() {
+		this.localidadesBusqueda = new HashSet<String>();
+	}
+
+	public UsuarioDAO(String nombre, String email, String password) {
+		super();
+		this.nombre = nombre;
+		this.email = email;
+		this.password = password;
+	}
 
 	public String getNombre() {
 		return nombre;
@@ -41,7 +53,7 @@ public class UsuarioDAO {
 	}
 
 	public boolean añadirLocalidad(String s) {
-		if (localidadesBusqueda.contains(s)) {
+		if (this.localidadesBusqueda.contains(s)) {
 			return false;
 		}
 		this.localidadesBusqueda.add(s);
@@ -49,7 +61,7 @@ public class UsuarioDAO {
 	}
 
 	public boolean quitarLocalidad(String s) {
-		if (!localidadesBusqueda.contains(s)) {
+		if (!this.localidadesBusqueda.contains(s)) {
 			return false;
 		}
 		this.localidadesBusqueda.remove(s);
@@ -57,7 +69,7 @@ public class UsuarioDAO {
 	}
 
 	public boolean añadirPosicion(PosicionDAO p) {
-		if (posicionesBusqueda.contains(p)) {
+		if (this.posicionesBusqueda.contains(p)) {
 			return false;
 		}
 		this.posicionesBusqueda.add(p);
@@ -65,31 +77,34 @@ public class UsuarioDAO {
 	}
 
 	public boolean quitarPosicion(PosicionDAO p) {
-		if (!posicionesBusqueda.contains(p)) {
+		if (!this.posicionesBusqueda.contains(p)) {
 			return false;
 		}
 		this.posicionesBusqueda.remove(p);
 		return true;
 	}
 
-	public TreeSet<ListaEESSPrecio> listarMisGasolineras() {
-
+	public TreeSet<Gasolinera> listarMisGasolineras() {
+		TreeSet<Gasolinera> gasolineras = new TreeSet<Gasolinera>();
 		try {
-			// Obtenemos los datos de la web del Ministerio y los guardamos en un String
+			// Obtenemos los datos de la web del Ministerio y los almacenamos en un String
 			String json = (HttpGet.httpGet(
 					"https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/"));
 			// Creamos un objeto Gson
 			Gson gson = new Gson();
 			// Convertimos el json en objeto (clase Gasolineras.class)
-			Gasolineras datos = gson.fromJson(json, Gasolineras.class);
-
+			ListadoGasolineras datosGasolineras = gson.fromJson(json, ListadoGasolineras.class);
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 		return gasolineras;
-
 	}
 
 	@Override
