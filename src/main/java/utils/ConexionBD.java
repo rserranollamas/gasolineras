57bbad4a184663992c739f5fd94230b43f63ad4d
rserranollamas <92532301+rserranollamas@users.noreply.github.com>
2022-05-27 15:20:59 +0200

    Clase ConexionBD creada
    Se crea dentro del paquete utils  la clase ConexionBD, que se encargará de realizar las conexiones y desconexiones con la Base de datos

---
+++ src/main/java/utils/ConexionBD.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class ConexionBD {
+	private final static String cadenaConexion="jdbc:mysql://127.0.0.1:3306/db1daw";
+	private final static String usuarioBD="1daw";
+	private final static String passwordBD="1daw";
+	private static Connection conexion; //singleton
+	
+	public static Statement conectar() {
+		try {
+			if (conexion == null) {
+				conexion=DriverManager.getConnection(
+						cadenaConexion,usuarioBD,passwordBD);	
+			}
+			return conexion.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void desconectar() {
+		if(conexion!=null) {
+			try {
+				conexion.close();
+				conexion=null;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	
+		}
+	}
+}
