c0294fc3f45f56d11f8644a937f4f9d34396b31e
rserranollamas <92532301+rserranollamas@users.noreply.github.com>
2022-06-05 10:42:09 +0200

    gasolineras.sql creado
    Se crea el sql para generar la base de datos gasolineras

---
+++ gasolineras.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS gasolineras;
+CREATE DATABASE gasolineras;
+
+USE gasolineras;
+
+CREATE TABLE usuario (
+  `dni` VARCHAR(9) PRIMARY KEY,
+  `nombre` VARCHAR(80) NOT NULL,
+  `email` VARCHAR(200) ,
+  `password` VARCHAR(20) NOT NULL
+);
+  
+CREATE TABLE posicion (
+`latitud` FLOAT(8,6),
+`longitud` FLOAT(9,6),
+`radioKm` INT(4),
+`usuario` VARCHAR(9),
+PRIMARY KEY(latitud,longitud,usuario),
+FOREIGN KEY(usuario) REFERENCES usuario(dni)
+);
+
+CREATE TABLE localidad (
+`nombre` VARCHAR(80),
+`usuario` VARCHAR(9),
+PRIMARY KEY(nombre,usuario), 
+FOREIGN KEY(usuario) REFERENCES usuario(dni)
+);
