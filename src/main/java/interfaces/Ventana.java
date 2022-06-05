10820d46846932c6ff5e44a19234efcfc3837469
rserranollamas <rserranollamas@gmail.com>
2022-06-05 22:52:50 +0200

    Clase Ventana modificada
    Se modifica la función irAPantalla  para que reciba por argumentos el Enum del nombre de la pantalla.

--- src/main/java/interfaces/Ventana.java
+++ src/main/java/interfaces/Ventana.java
@@ -3,33 +3,40 @@ package interfaces;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import clases.UsuarioDAO;
+import enums.NombrePantalla;
+
 public class Ventana extends JFrame {
 	/** Representa la pantalla actual por la que estoy navegando **/
-	private JPanel ventanaActual;
-
+	private JPanel pantallaActual;
+	protected UsuarioDAO usuarioLogado;
+	
 	public Ventana() {
 		this.setSize(600, 600);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Gasolineras");
+		this.pantallaActual=new PantallaInicio(this);
+		this.setContentPane(this.pantallaActual);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
-
 		this.setVisible(true);
 	}
 
-	public void irAVentana(String nombreVentana) {
-		this.ventanaActual.setVisible(false);
-		this.ventanaActual = null;
+	public void irAPantalla(NombrePantalla nombreVentana) {
+		this.pantallaActual.setVisible(false);
+		this.pantallaActual = null;
 		switch (nombreVentana) {
-		case "login":
-		//	this.ventanaActual=new VentanaLogin(this);
+		case INICIO:
+			this.pantallaActual=new PantallaInicio(this);
 			break;
-		case "registro":
-		//	this.ventanaActual=new VentanaRegistro(this);
+		case REGISTRO:
+			this.pantallaActual=new PantallaRegistro(this);
+			break;
+		case LOGIN:
+			this.pantallaActual=new PantallaLogin(this);
 			break;
 		}
-		
-		this.ventanaActual.setVisible(true);
-		this.setContentPane(this.ventanaActual);
+		this.pantallaActual.setVisible(true);
+		this.setContentPane(this.pantallaActual);
 	}
 }
