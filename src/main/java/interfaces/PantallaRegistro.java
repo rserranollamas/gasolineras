85a38f6cb11e84203ad86b43bc7c26b595223e9b
rserranollamas <rserranollamas@gmail.com>
2022-06-05 22:59:18 +0200

    Clase PantallaRegistro creada
    Se crea la clase PantallaRegistro donde un nuevo usuario podrá registrarse para poder usar la aplicación.

---
+++ src/main/java/interfaces/PantallaRegistro.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import javax.swing.JPanel;
+
+public class PantallaRegistro extends JPanel {
+	private Ventana ventana;
+
+	public PantallaRegistro(Ventana v) {
+		this.ventana = v;
+	}
+
+}
