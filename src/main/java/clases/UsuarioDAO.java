d1b78c727ab353a01c5bf2d86217320a5c4f4fd7
rserranollamas <92532301+rserranollamas@users.noreply.github.com>
2022-06-05 08:52:59 +0200

    Clase UsuarioDAO modificada
    Se añaden las sentencias SQL (select, insert, update) para convertir la clase en DAO, se realiza tanto en los constructores como en los setters.
    También se añaden las excepciones creadas

--- src/main/java/clases/UsuarioDAO.java
+++ src/main/java/clases/UsuarioDAO.java
@@ -3,15 +3,22 @@ package clases;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TreeSet;
 
 import com.google.gson.Gson;
 
+import excepciones.PassIncorrectoException;
+import excepciones.UsuarioNoExisteException;
+import utils.ConexionBD;
 import utils.HttpGet;
 
 public class UsuarioDAO {
+	private String dni;
 	private String nombre;
 	private String email;
 	private String password;
@@ -21,37 +28,127 @@ public class UsuarioDAO {
 	public UsuarioDAO() {
 	}
 
-	public UsuarioDAO(String nombre, String email, String password) {
-		super();
-		this.nombre = nombre;
-		this.email = email;
-		this.password = password;
-		this.localidadesBusqueda = new HashSet<String>();
-		this.posicionesBusqueda = new HashSet<PosicionDAO>();
+	/**
+	 * Este constructor va hacer que persista en BBDD el usuario que estoy creando
+	 * 
+	 * @param dni      dni del usuario
+	 * @param nombre   nombre del usuario
+	 * @param email    email del usuario
+	 * @param password password del usuario
+	 * @throws SQLException
+	 */
+	public UsuarioDAO(String dni, String nombre, String email, String password) throws SQLException {
+
+		Statement smt = ConexionBD.conectar();
+		if (smt.executeUpdate(
+				"INSERT INTO usuario VALUES('" + dni + "','" + nombre + "','" + email + "','" + password + "')") > 0) {
+			// Solo si todo ha ido bien insertando, se modifican las variables internas
+			this.dni = dni;
+			this.nombre = nombre;
+			this.email = email;
+			this.password = password;
+			this.localidadesBusqueda = new HashSet<String>();
+			this.posicionesBusqueda = new HashSet<PosicionDAO>();
+		} else {
+			// Si no se ha podido insertar, lanzo un error: No se ha podido insertar.
+			ConexionBD.desconectar();
+			throw new SQLException("No se ha podido insertar al usuario en la BBDD");
+		}
+		ConexionBD.desconectar();
+
+	}
+
+	/**
+	 * Este constructor va a consultar en la BBDD al usuario a partir de su DNI y passwotd
+	 * @param dni dni del usuario
+	 * @param password password del usuario
+	 * @throws SQLException
+	 * @throws PassIncorrectoException
+	 * @throws UsuarioNoExisteException
+	 */
+
+	public UsuarioDAO(String dni, String password)
+			throws SQLException, PassIncorrectoException, UsuarioNoExisteException {
+		Statement smt = ConexionBD.conectar();
+		ResultSet cursor = smt.executeQuery("SELECT * FROM usuario WHERE dni='" + dni + "'");
+		if (cursor.next()) {
+			this.password = cursor.getString("password");
+			if (!this.password.equals(password)) {
+				ConexionBD.desconectar();
+				throw new PassIncorrectoException("El password es incorrecto");
+			}
+			this.dni = cursor.getString("dni");
+			this.nombre = cursor.getString("nombre");
+			this.email = cursor.getString("email");
+			this.localidadesBusqueda = new HashSet<String>();
+			this.posicionesBusqueda = new HashSet<PosicionDAO>();
+
+		} else {
+			ConexionBD.desconectar();
+			throw new UsuarioNoExisteException("No existe ningún usuario con el DNI indicado");
+
+		}
+		ConexionBD.desconectar();
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) throws SQLException {	
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("UPDATE usuario SET dni='"+dni+"' WHERE dni='"+this.dni+"'")>0) {
+			this.dni = dni;
+		}else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se ha podido modificar el DNI");
+		}
+		ConexionBD.desconectar();	
 	}
 
 	public String getNombre() {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
+	public void setNombre(String nombre) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("UPDATE usuario SET nombre='"+nombre+"' WHERE dni='"+this.dni+"'")>0) {
+			this.nombre = nombre;
+		}else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se ha podido modificar el nombre");
+		}
+		ConexionBD.desconectar();
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setEmail(String email) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("UPDATE usuario SET email='"+email+"' WHERE dni='"+this.dni+"'")>0) {
+			this.email = email;
+		}else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se ha podido modificar el email");
+		}
+		ConexionBD.desconectar();	
 	}
 
 	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setPassword(String password) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("UPDATE usuario SET password='"+password+"' WHERE dni='"+this.dni+"'")>0) {
+			this.password = password;
+		}else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se ha podido modificar el password");
+		}
+		ConexionBD.desconectar();
 	}
 
 	public boolean añadirLocalidad(String s) {
@@ -70,21 +167,28 @@ public class UsuarioDAO {
 		return true;
 	}
 
-	public boolean añadirPosicion(PosicionDAO p) {
+	public boolean añadirPosicion(PosicionDAO p) throws SQLException {
 		if (this.posicionesBusqueda.contains(p)) {
 			return false;
 		}
 		this.posicionesBusqueda.add(p);
-		return true;
+		return true;	
 	}
 
 	public boolean quitarPosicion(PosicionDAO p) {
 		if (!this.posicionesBusqueda.contains(p)) {
 			return false;
 		}
+		// Se elimina la posicion del HashSet posicionesBusqueda
 		this.posicionesBusqueda.remove(p);
+		// Se elimina la posicion de la BBDD
+		p.eliminar();
 		return true;
 	}
+	/**
+	 * 
+	 * @return devuelve el TreeSet con las gasolineras
+	 */
 
 	public TreeSet<Gasolinera> listarMisGasolineras() {
 		TreeSet<Gasolinera> gasolineras = new TreeSet<Gasolinera>();
