894e452d36da08ab323d6ada1b588cd26f26c99c
rserranollamas <rserranollamas@gmail.com>
2022-06-08 23:12:23 +0200

    Interfaz PantallaGasolineras creada
    Se crea nueva interfaz PantallaGasolineras que mostrará todas las gasolineras que ha encontrado en la búsqueda

---
+++ src/main/java/interfaces/PantallaGasolineras.java
@@ -0,0 +1,48 @@
+package interfaces;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import javax.swing.SwingConstants;
+
+import clases.Gasolinera;
+import componentesvisuales.ElementoListaGasolineras;
+
+import javax.swing.JScrollPane;
+import javax.swing.BoxLayout;
+
+public class PantallaGasolineras extends JPanel {
+	private Ventana ventana;
+
+	public PantallaGasolineras(Ventana v, String mensaje) {
+		this.ventana = v;
+		setLayout(new BorderLayout(0, 0));
+
+		JLabel labelTitulo = new JLabel("LISTA DE GASOLINERAS");
+		labelTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitulo.setFont(new Font("Bauhaus 93", Font.PLAIN, 30));
+		add(labelTitulo, BorderLayout.NORTH);
+
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+
+		JPanel listadoGasolineras = new JPanel();
+		scrollPane.setViewportView(listadoGasolineras);
+		listadoGasolineras.setLayout(new BoxLayout(listadoGasolineras, BoxLayout.Y_AXIS));
+
+		TreeSet<Gasolinera> gasolineras = ventana.usuarioLogado.listarMisGasolineras();
+
+		Iterator it = gasolineras.iterator();
+		while (it.hasNext()) {
+			Gasolinera gasolinera = (Gasolinera) it.next();
+			System.out.println(gasolinera);
+			listadoGasolineras.add(new ElementoListaGasolineras(ventana, gasolinera));
+		}
+
+	}
+
+}
