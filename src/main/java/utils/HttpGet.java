e43543c3a86cf31c8bbc83da95771e357e4786a2
rserranollamas <rserranollamas@gmail.com>
2022-06-09 13:35:05 +0200

    Clase HttpGet modificada
    Se comenta el FIX para realizar la conexión sin SSL  por si alguna vez se ejecuta en algún equipo Mac poder activar de nuevo el FIX.

--- src/main/java/utils/HttpGet.java
+++ src/main/java/utils/HttpGet.java
@@ -19,26 +19,26 @@ import javax.net.ssl.X509TrustManager;
 public class HttpGet {
 	public static String httpGet(String urlStr) throws IOException, KeyManagementException, NoSuchAlgorithmException {
 		/* Start of the fix */
-	/*
-		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
-            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
-            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+		/*
+		 * TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+		 * public java.security.cert.X509Certificate[] getAcceptedIssuers() { return
+		 * null; } public void checkClientTrusted(X509Certificate[] certs, String
+		 * authType) { } public void checkServerTrusted(X509Certificate[] certs, String
+		 * authType) { }
+		 * 
+		 * } };
+		 * 
+		 * SSLContext sc = SSLContext.getInstance("SSL"); sc.init(null, trustAllCerts,
+		 * new java.security.SecureRandom());
+		 * HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+		 * 
+		 * // Create all-trusting host name verifier HostnameVerifier allHostsValid =
+		 * new HostnameVerifier() { public boolean verify(String hostname, SSLSession
+		 * session) { return true; } }; // Install the all-trusting host verifier
+		 * HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid); /* End of the
+		 * fix
+		 */
 
-        } };
-
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        // Create all-trusting host name verifier
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) { return true; }
-        };
-        // Install the all-trusting host verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-        /* End of the fix */
-	 
 		// Creamos un objeto de tipo URL
 		URL url = new URL(urlStr);
 		// Abrimos la conexión e indicamos que será de tipo GET HttpURLConnection
@@ -49,20 +49,22 @@ public class HttpGet {
 		// excepción con el código de respuesta
 		if (conexion.getResponseCode() != 200) {
 			throw new IOException(conexion.getResponseMessage());
+		} else {
+			// Creamos el buffer para leer BufferedReader
+			BufferedReader lector = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+			StringBuilder resultado = new StringBuilder();
+			String linea = lector.readLine();
+			// Mientras el BufferedReader se pueda leer, va añadiendo contenido a resultado
+			while (linea != null) {
+				resultado.append(linea);
+				linea = lector.readLine();
+			}
+			// Cerramos el BufferedReader y la conexión
+			lector.close();
+			conexion.disconnect();
+			// Devolvemos el resultado como cadena, no como StringBuilder
+			return resultado.toString();
 		}
-		// Creamos el buffer para leer BufferedReader
-		BufferedReader lector = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
-		StringBuilder resultado = new StringBuilder();
-		String linea = lector.readLine();
-		// Mientras el BufferedReader se pueda leer, va añadiendo contenido a resultado
-		while (linea != null) {
-			resultado.append(linea);
-			linea = lector.readLine();
-		}
-		// Cerramos el BufferedReader y la conexión
-		lector.close();
-		conexion.disconnect();
-		// Devolvemos el resultado como cadena, no como StringBuilder
-		return resultado.toString();
+
 	}
 }
