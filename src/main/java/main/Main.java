61a335b951edc95d1e2d2f9037cdcd573b6b9c7c
rserranollamas <rserranollamas@gmail.com>
2022-06-09 13:52:11 +0200

    Clase Main
    Clase Main donde se crea un nuevo objeto Ventana, también se añade UIManager.setLookAndFeel para darle otro estilo a la ventana

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,54 @@
 package main;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 import com.google.gson.Gson;
 
+import clases.UsuarioDAO;
+import excepciones.PassIncorrectoException;
+import excepciones.UsuarioNoExisteException;
 import interfaces.Ventana;
 import utils.HttpGet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Ventana ventana=new Ventana();
+		
 
+		try {
+			JFrame.setDefaultLookAndFeelDecorated(true);
+			JDialog.setDefaultLookAndFeelDecorated(true);
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Ventana ventana = new Ventana();
+		
+	/*
+		try {
+			UsuarioDAO usuario = new UsuarioDAO("75897420L");
+			System.out.println(usuario.getEmail());
+		} catch (SQLException | UsuarioNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			UsuarioDAO usuario2=new UsuarioDAO("75897420L","holahola");
+		} catch (SQLException | PassIncorrectoException | UsuarioNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	*/
 	}
 
 }
