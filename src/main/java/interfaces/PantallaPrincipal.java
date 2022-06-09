48ea2e27c7efd21289d99d6eec983716fbde3726
rserranollamas <rserranollamas@gmail.com>
2022-06-09 13:36:40 +0200

    Interfaz PantallaPrincipal actualizada
    Se añaden nuevos botones poder para acceder a nuevas pantallas creadas.

--- src/main/java/interfaces/PantallaPrincipal.java
+++ src/main/java/interfaces/PantallaPrincipal.java
@@ -3,9 +3,12 @@ package interfaces;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 
+import clases.PosicionDAO;
 import enums.NombrePantalla;
+import excepciones.PosicionNoExisteException;
 
 import javax.swing.BoxLayout;
 import java.awt.GridBagLayout;
@@ -13,10 +16,15 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.SQLException;
 
 public class PantallaPrincipal extends JPanel {
 	private Ventana ventana;
 	protected JPanel contenedor;
+	protected JButton botonLogin;
+	protected JButton botonMiPerfil;
+	protected JButton botonUbicacion;
+	protected JButton botonLocalidades;
 
 	public PantallaPrincipal(Ventana v) {
 		this.ventana = v;
@@ -31,16 +39,17 @@ public class PantallaPrincipal extends JPanel {
 		add(panelEste, BorderLayout.WEST);
 		GridBagLayout gbl_panelEste = new GridBagLayout();
 		gbl_panelEste.columnWidths = new int[] { 113, 0 };
-		gbl_panelEste.rowHeights = new int[] { 23, 23, 23, 0, 0, 0 };
+		gbl_panelEste.rowHeights = new int[] { 23, 23, 0, 0, 23, 0, 0, 0 };
 		gbl_panelEste.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-		gbl_panelEste.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelEste.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panelEste.setLayout(gbl_panelEste);
 
-		JButton botonLogin = new JButton("INICIAR SESIÓN");
+		botonLogin = new JButton("INICIAR SESIÃ“N");
 		botonLogin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				ventana.irAPantalla(NombrePantalla.INICIO, "INICIAR SESIÓN O REGISTRARSE");
+				ventana.irAPantalla(NombrePantalla.INICIO, "INICIAR SESIÃ“N O REGISTRARSE");
+				botonLogin.setText("INICIAR SESIÃ“N");
 			}
 		});
 		GridBagConstraints gbc_botonLogin = new GridBagConstraints();
@@ -50,11 +59,13 @@ public class PantallaPrincipal extends JPanel {
 		gbc_botonLogin.gridy = 0;
 		panelEste.add(botonLogin, gbc_botonLogin);
 
-		JButton botonMiPerfil = new JButton("MI PERFIL");
+		botonMiPerfil = new JButton("MI PERFIL");
+		botonMiPerfil.setEnabled(false);
 		botonMiPerfil.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-
+				ventana.irAPantalla(NombrePantalla.MIPERFIL,
+						"PERFIL DE " + ventana.usuarioLogado.getNombre().toUpperCase());
 			}
 		});
 		botonMiPerfil.setVerticalAlignment(SwingConstants.TOP);
@@ -65,28 +76,82 @@ public class PantallaPrincipal extends JPanel {
 		gbc_botonMiPerfil.gridy = 1;
 		panelEste.add(botonMiPerfil, gbc_botonMiPerfil);
 
-		JButton botonBuscar = new JButton("BUSCAR");
-		GridBagConstraints gbc_botonBuscar = new GridBagConstraints();
-		gbc_botonBuscar.insets = new Insets(0, 0, 5, 0);
-		gbc_botonBuscar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_botonBuscar.gridx = 0;
-		gbc_botonBuscar.gridy = 2;
-		panelEste.add(botonBuscar, gbc_botonBuscar);
-
-		JButton botonUbicacion = new JButton("UBICACIÓN");
+		botonUbicacion = new JButton("UBICACIÃ“N");
+		botonUbicacion.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					ventana.posicionUsuario = new PosicionDAO(ventana.usuarioLogado);
+					System.out.println(ventana.usuarioLogado.getDni());
+					ventana.irAPantalla(NombrePantalla.UBICACION, "Puedes modificar la ubicaciÃ³n si lo deseas");
+					ventana.pantallaUbicacion.campoLatitud
+							.setText(Float.toString(ventana.posicionUsuario.getLatitud()));
+					ventana.pantallaUbicacion.campoLongitud
+							.setText(Float.toString(ventana.posicionUsuario.getLongitud()));
+					ventana.pantallaUbicacion.campoRadioKm
+							.setText(Short.toString(ventana.posicionUsuario.getRadioKm()));
+				} catch (SQLException | PosicionNoExisteException e1) {
+					ventana.irAPantalla(NombrePantalla.UBICACION,
+							"Introduce tu ubicaciï¿½n para poder realizar la bï¿½squeda");
+					JOptionPane.showMessageDialog(ventana, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+					e1.printStackTrace();
+				}
+			}
+		});
+		botonUbicacion.setEnabled(false);
 		GridBagConstraints gbc_botonUbicacion = new GridBagConstraints();
 		gbc_botonUbicacion.insets = new Insets(0, 0, 5, 0);
 		gbc_botonUbicacion.fill = GridBagConstraints.HORIZONTAL;
 		gbc_botonUbicacion.gridx = 0;
-		gbc_botonUbicacion.gridy = 3;
+		gbc_botonUbicacion.gridy = 2;
 		panelEste.add(botonUbicacion, gbc_botonUbicacion);
 
+		JButton botonBuscar = new JButton("BUSCAR");
+		botonBuscar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ventana.irAPantalla(NombrePantalla.GASOLINERAS, "Listado de Gasolineras");
+			}
+		});
+
 		JButton botonCombustible = new JButton("COMBUSTIBLE");
+		botonCombustible.setEnabled(false);
+		botonCombustible.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ventana.irAPantalla(NombrePantalla.COMBUSTIBLE,
+						ventana.usuarioLogado.getNombre().toUpperCase() + ", SELECCIONA EL COMBUSTIBLE ");
+			}
+		});
+
+		botonLocalidades = new JButton("LOCALIDADES");
+		botonLocalidades.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ventana.irAPantalla(NombrePantalla.LOCALIDADES,
+						ventana.usuarioLogado.getNombre().toUpperCase() + ", SELECCIONA LOCALIDADES A Aï¿½ADIR");
+			}
+		});
+		botonLocalidades.setEnabled(false);
+		GridBagConstraints gbc_botonLocalidades = new GridBagConstraints();
+		gbc_botonLocalidades.insets = new Insets(0, 0, 5, 0);
+		gbc_botonLocalidades.fill = GridBagConstraints.HORIZONTAL;
+		gbc_botonLocalidades.gridx = 0;
+		gbc_botonLocalidades.gridy = 3;
+		panelEste.add(botonLocalidades, gbc_botonLocalidades);
 		GridBagConstraints gbc_botonCombustible = new GridBagConstraints();
+		gbc_botonCombustible.insets = new Insets(0, 0, 5, 0);
 		gbc_botonCombustible.fill = GridBagConstraints.HORIZONTAL;
 		gbc_botonCombustible.gridx = 0;
 		gbc_botonCombustible.gridy = 4;
 		panelEste.add(botonCombustible, gbc_botonCombustible);
+		botonBuscar.setEnabled(false);
+		GridBagConstraints gbc_botonBuscar = new GridBagConstraints();
+		gbc_botonBuscar.insets = new Insets(0, 0, 5, 0);
+		gbc_botonBuscar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_botonBuscar.gridx = 0;
+		gbc_botonBuscar.gridy = 5;
+		panelEste.add(botonBuscar, gbc_botonBuscar);
 
 	}
 }
