baca96f317b961c4f67dc858afe7e2aeb86646f7
rserranollamas <rserranollamas@gmail.com>
2022-06-09 13:44:42 +0200

    Interfaz PantallaLocalidades creada
    Se crea nueva interfaz PantallaLocalidades que mostrará las localidades que hemos añadido para realizar la búsqueda de gasolineras.

---
+++ src/main/java/interfaces/PantallaLocalidades.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import javax.swing.JPanel;
+
+public class PantallaLocalidades extends JPanel {
+	private Ventana ventana;
+
+	public PantallaLocalidades(Ventana v, String mensaje) {
+		this.ventana=v;
+		setLayout(null);
+		
+	}
+
+}
