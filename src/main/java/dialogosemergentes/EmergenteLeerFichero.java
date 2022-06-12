48881ab0c2b5f2a0afa9bbd4b9852c2a636b5e17
rserranollamas <rserranollamas@gmail.com>
2022-06-13 00:58:10 +0200

    Diálogo emergente EmergenteLeerFichero modificado
    Se añade scrollPane y dentro se inserta el textPane, para que cuando el fichero contenga muchas gasolineras aparezca el scroll.
    Se añade mensaje para que nos informe cuando ha leído el fichero y cuando ha dado error.

--- src/main/java/dialogosemergentes/EmergenteLeerFichero.java
+++ src/main/java/dialogosemergentes/EmergenteLeerFichero.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 
 import interfaces.Ventana;
 import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
 public class EmergenteLeerFichero extends JDialog {
 	private Ventana ventana;
 	private JButton botonLeer;
 	private JTextPane textPane;
+	private JScrollPane scrollPane;
 
 	public EmergenteLeerFichero(Ventana v) {
 		this.ventana = v;
@@ -32,9 +35,13 @@ public class EmergenteLeerFichero extends JDialog {
 		labelCabecera.setBounds(174, 0, 398, 60);
 		getContentPane().add(labelCabecera);
 
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 58, 734, 116);
+		getContentPane().add(scrollPane);
+
 		textPane = new JTextPane();
 		textPane.setBounds(33, 51, 697, 123);
-		getContentPane().add(textPane);
+		scrollPane.setViewportView(textPane);
 
 		botonLeer = new JButton("Leer");
 		botonLeer.addActionListener(new ActionListener() {
@@ -49,14 +56,15 @@ public class EmergenteLeerFichero extends JDialog {
 						linea = lector.readLine();
 					}
 					lector.close();
-
+					JOptionPane.showMessageDialog(ventana, "Se ha leído el fichero correctamente", "Información",
+							JOptionPane.INFORMATION_MESSAGE);
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog(ventana, e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 					e1.printStackTrace();
 				}
 			}
 		});
-		botonLeer.setBounds(321, 185, 132, 36);
+		botonLeer.setBounds(269, 185, 132, 36);
 		getContentPane().add(botonLeer);
 
 	}
