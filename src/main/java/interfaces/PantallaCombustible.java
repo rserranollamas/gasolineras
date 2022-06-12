ed9f2ddbab542712058ad1af91ffee5219501135
rserranollamas <rserranollamas@gmail.com>
2022-06-12 07:10:32 +0200

    Interfaz PantallaCombustible modificada
    Se elimina el mensaje que recibía por parámetros para que solo reciba el objeto Ventana.
    Además, se añade un combobox que contiene los datos del enum TipoCombustible. Cuando se pulsa el botón buscar, a partir del elemento seleccionado del combobox va creando los ElementoListaGasolineras que muestran las gasolineras que están en el radio de la ubicación que se ha seleccionado y/o localidad/es añadidas a la búsqueda con el precio de la gasolina que se ha seleccionado.

--- src/main/java/interfaces/PantallaCombustible.java
+++ src/main/java/interfaces/PantallaCombustible.java
@@ -2,91 +2,143 @@ package interfaces;
 
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import javax.swing.JOptionPane;
+
+import clases.Gasolinera;
+import componentesvisuales.ElementoListaGasolineras;
+
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import enums.TipoCombustible;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
+import java.util.TreeSet;
 import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JTextArea;
-import java.awt.GridBagConstraints;
-import javax.swing.JRadioButton;
-import java.awt.Insets;
+import javax.swing.JScrollPane;
 
 public class PantallaCombustible extends JPanel {
 	private Ventana ventana;
+	private JPanel listadoGasolineras;
 
-	public PantallaCombustible(Ventana v, String mensaje) {
+	public PantallaCombustible(Ventana v) {
 		this.ventana = v;
 		setLayout(new BorderLayout(0, 0));
-		
-		JLabel labelCabecera = new JLabel(mensaje);
-		labelCabecera.setHorizontalAlignment(SwingConstants.CENTER);
-		labelCabecera.setFont(new Font("Tahoma", Font.BOLD, 24));
-		add(labelCabecera, BorderLayout.NORTH);
-		
-		JPanel panel = new JPanel();
-		add(panel, BorderLayout.CENTER);
-		panel.setLayout(null);
-		
-		JRadioButton combustibleGasolina95 = new JRadioButton("Gasolina 95");
-		combustibleGasolina95.setBounds(102, 31, 170, 23);
-		panel.add(combustibleGasolina95);
-		
-		JRadioButton combustibleGasoleoB = new JRadioButton("Gas\u00F3leo B");
-		combustibleGasoleoB.setBounds(290, 31, 150, 23);
-		panel.add(combustibleGasoleoB);
-		
-		JRadioButton combustibleGasolina95Premium = new JRadioButton("Gasolina 95 Premium");
-		combustibleGasolina95Premium.setBounds(102, 59, 170, 23);
-		panel.add(combustibleGasolina95Premium);
-		
-		JRadioButton combustibleBiodiesel = new JRadioButton("Biodi\u00E9sel");
-		combustibleBiodiesel.setBounds(290, 59, 150, 23);
-		panel.add(combustibleBiodiesel);
-		
-		JRadioButton combustibleGasolina98 = new JRadioButton("Gasolina 98");
-		combustibleGasolina98.setBounds(102, 87, 170, 23);
-		panel.add(combustibleGasolina98);
-		
-		JRadioButton combustibleBioetanol = new JRadioButton("Bioetanol");
-		combustibleBioetanol.setBounds(290, 87, 150, 23);
-		panel.add(combustibleBioetanol);
-		
-		JRadioButton combustibleDiesel = new JRadioButton("Di\u00E9sel");
-		combustibleDiesel.setBounds(102, 115, 170, 23);
-		panel.add(combustibleDiesel);
-		
-		JRadioButton combustibleGlp = new JRadioButton("GLP");
-		combustibleGlp.setBounds(290, 115, 150, 23);
-		panel.add(combustibleGlp);
-		
-		JRadioButton combustibleDieselPremium = new JRadioButton("Di\u00E9sel Premium");
-		combustibleDieselPremium.setBounds(102, 143, 170, 23);
-		panel.add(combustibleDieselPremium);
-		
-		JRadioButton combustibleGnc = new JRadioButton("GNC");
-		combustibleGnc.setBounds(290, 143, 150, 23);
-		panel.add(combustibleGnc);
-		
-		// Agrupamos los botones de combustible
-		ButtonGroup grupoCombustible=new ButtonGroup();
-		grupoCombustible.add(combustibleGnc);
-		grupoCombustible.add(combustibleDieselPremium);
-		grupoCombustible.add(combustibleGlp);
-		grupoCombustible.add(combustibleDiesel);
-		grupoCombustible.add(combustibleBioetanol);
-		grupoCombustible.add(combustibleGasolina98);
-		grupoCombustible.add(combustibleBiodiesel);
-		grupoCombustible.add(combustibleGasolina95Premium);
-		grupoCombustible.add(combustibleGasoleoB);
-		grupoCombustible.add(combustibleGasolina95);
+
+		JPanel panelCabecera = new JPanel();
+		add(panelCabecera, BorderLayout.NORTH);
+
+		final JComboBox seleccionCombustible = new JComboBox();
+		panelCabecera.add(seleccionCombustible);
+		seleccionCombustible.setModel(new DefaultComboBoxModel(TipoCombustible.values()));
+
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+
+		listadoGasolineras = new JPanel();
+		scrollPane.setViewportView(listadoGasolineras);
+		listadoGasolineras.setLayout(new BoxLayout(listadoGasolineras, BoxLayout.Y_AXIS));
+
+		JButton botonBuscar = new JButton("Buscar");
+		panelCabecera.add(botonBuscar);
+		botonBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				listadoGasolineras.removeAll();
+				switch ((TipoCombustible) seleccionCombustible.getSelectedItem()) {
+				case GASOLINA_95:
+					mostrarGasolineras(TipoCombustible.GASOLINA_95);
+					break;
+				case GASOLINA_95_PREMIUM:
+					mostrarGasolineras(TipoCombustible.GASOLINA_95_PREMIUM);
+					break;
+				case GASOLINA_98:
+					mostrarGasolineras(TipoCombustible.GASOLINA_98);
+					break;
+				case DIESEL:
+					mostrarGasolineras(TipoCombustible.DIESEL);
+					break;
+				case DIESEL_PREMIUM:
+					mostrarGasolineras(TipoCombustible.DIESEL_PREMIUM);
+					break;
+				case GASOLEO_B:
+					mostrarGasolineras(TipoCombustible.GASOLEO_B);
+					break;
+				case BIODIESEL:
+					mostrarGasolineras(TipoCombustible.BIODIESEL);
+					;
+					break;
+				case BIOETANOL:
+					mostrarGasolineras(TipoCombustible.BIOETANOL);
+					break;
+				case GLP:
+					mostrarGasolineras(TipoCombustible.GLP);
+					break;
+				case GNC:
+					mostrarGasolineras(TipoCombustible.GNC);
+					break;
+				}
+				listadoGasolineras.repaint();
+				listadoGasolineras.revalidate();
+			}
+		});
 
 	}
 
+	public void mostrarGasolineras(TipoCombustible combustibleSeleccionado) {
+		try {
+			String precio = null;
+			TreeSet<Gasolinera> gasolineras = ventana.usuarioLogado.listarMisGasolineras();
+			Iterator it = gasolineras.iterator();
+			while (it.hasNext()) {
+				Gasolinera gasolinera = (Gasolinera) it.next();
+				switch (combustibleSeleccionado) {
+				case GASOLINA_95:
+					precio = gasolinera.getGasolina95E5();
+					break;
+				case GASOLINA_95_PREMIUM:
+					precio = gasolinera.getGasolina95E5Premium();
+					break;
+				case GASOLINA_98:
+					precio = gasolinera.getGasolina98E5();
+					break;
+				case DIESEL:
+					precio = gasolinera.getGasoleoA();
+					break;
+				case DIESEL_PREMIUM:
+					precio = gasolinera.getGasoleoPremium();
+					break;
+				case GASOLEO_B:
+					precio = gasolinera.getGasoleoB();
+					break;
+				case BIODIESEL:
+					precio = gasolinera.getBioDiesel();
+					break;
+				case BIOETANOL:
+					precio = gasolinera.getBioDiesel();
+					break;
+				case GLP:
+					precio = gasolinera.getGlp();
+					break;
+				case GNC:
+					precio = gasolinera.getGnc();
+					break;
+				default:
+					break;
+				}
+				if (!precio.isEmpty()) {
+					listadoGasolineras.add(new ElementoListaGasolineras(ventana, gasolinera, precio));
+				}
+			}
+		} catch (KeyManagementException | NoSuchAlgorithmException | IOException e) {
+			JOptionPane.showMessageDialog(ventana, "Error al conectar:\n" + e.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+
+	}
 }
