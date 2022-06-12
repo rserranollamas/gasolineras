83e91db39aeadd52765025aab22d0eaf806d9755
rserranollamas <rserranollamas@gmail.com>
2022-06-12 07:17:16 +0200

    Interfaz Ventana modificada
    Se modifica la interfaz Ventana, cambiando el tamaño de la ventana para que el resultado de la búsqueda de gasolineras se vea mejor.
    Además, se modifican los irAPantalla, ya que antes al crear cada pantalla se enviaba un objeto Ventana y un mensaje, y ahora solo vamos a enviar el objeto ventana.

--- src/main/java/interfaces/Ventana.java
+++ src/main/java/interfaces/Ventana.java
@@ -3,7 +3,6 @@ package interfaces;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import clases.PosicionDAO;
 import clases.UsuarioDAO;
@@ -12,12 +11,11 @@ import enums.NombrePantalla;
 public class Ventana extends JFrame {
 	/** Representa la pantalla actual por la que estoy navegando **/
 	protected PantallaPrincipal pantallaPrincipal;
-	protected PantallaUbicacion pantallaUbicacion;
 	protected UsuarioDAO usuarioLogado;
 	protected PosicionDAO posicionUsuario;
 
 	public Ventana() {
-		this.setSize(700, 400);
+		this.setSize(800, 400);
 		this.setLocationRelativeTo(null);
 		this.setTitle("Gasolineras");
 		this.pantallaPrincipal = new PantallaPrincipal(this);
@@ -28,39 +26,34 @@ public class Ventana extends JFrame {
 		this.setVisible(true);
 	}
 
-	public void irAPantalla(NombrePantalla nombreVentana, String mensaje) {
-		this.pantallaPrincipal.contenedor.setLocation(0, 0);
+	public void irAPantalla(NombrePantalla nombreVentana) {
 		this.pantallaPrincipal.contenedor.removeAll();
 		switch (nombreVentana) {
 		case PRINCIPAL:
 			break;
 		case INICIO:
-			this.pantallaPrincipal.contenedor.add(new PantallaInicio(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaInicio(this), BorderLayout.CENTER);
 			break;
 		case REGISTRO:
-			this.pantallaPrincipal.contenedor.add(new PantallaRegistro(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaRegistro(this), BorderLayout.CENTER);
 			break;
 		case LOGIN:
-			this.pantallaPrincipal.contenedor.add(new PantallaLogin(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaLogin(this), BorderLayout.CENTER);
 			break;
 		case BIENVENIDA:
-			this.pantallaPrincipal.contenedor.add(new PantallaBienvenida(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaBienvenida(this), BorderLayout.CENTER);
 			break;
 		case COMBUSTIBLE:
-			this.pantallaPrincipal.contenedor.add(new PantallaCombustible(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaCombustible(this), BorderLayout.CENTER);
 			break;
 		case MIPERFIL:
-			this.pantallaPrincipal.contenedor.add(new PantallaMiPerfil(this, mensaje), BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaMiPerfil(this), BorderLayout.CENTER);
 			break;
 		case UBICACION:
-			this.pantallaUbicacion = new PantallaUbicacion(this, mensaje);
-			this.pantallaPrincipal.contenedor.add(this.pantallaUbicacion, BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaUbicacion(this), BorderLayout.CENTER);
 			break;
 		case LOCALIDADES:
-			this.pantallaPrincipal.contenedor.add(new PantallaLocalidades(this, mensaje), BorderLayout.CENTER);
-			break;
-		case GASOLINERAS:
-			this.pantallaPrincipal.contenedor.add(new PantallaGasolineras(this,mensaje),BorderLayout.CENTER);
+			this.pantallaPrincipal.contenedor.add(new PantallaLocalidades(this), BorderLayout.CENTER);
 			break;
 		}
 		this.pantallaPrincipal.contenedor.repaint();
